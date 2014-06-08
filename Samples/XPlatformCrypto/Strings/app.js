5e60f286e989e3f44c7b00bcd3eaee8e1f016b6e
Ben Bahrenburg <ben.bahrenburg@gmail.com>
2014-06-08 13:29:37 -0700

    Add IV example

--- Samples/XPlatformCrypto/Strings/app.js
+++ Samples/XPlatformCrypto/Strings/app.js
@@ -4,6 +4,9 @@ var mod = require('bencoding.securely'),
 	plainText = "mary had a little lamb, little lamb, little lamb",
 	iOSOutput = "duSnwv3AGC5ICbn//3sjiL2L6kUNEMPjq+hniRdL9ow+QjGcM+2pBCw+3h19LAc3ugdEB5/ftwNVAO9sStSoNQ==";
 
+
+Ti.API.info("First without generating an IV");
+
 var encyptedText = crypto.encrypt(password,plainText);
 Ti.API.info('encyptedText = ' + encyptedText);
 
@@ -12,4 +15,21 @@ Ti.API.info('decryptedText = ' + decryptedText);
 Ti.API.info('Do they match? = ' + ((decryptedText == plainText) ? 'Yes' : 'No'));
 if(Ti.Platform.Android){
 	Ti.API.info('Output Cross Platform? = ' + ((crypto.decrypt(password, iOSOutput)==decryptedText) ? 'Yes' : 'No'));
+}
+
+
+Ti.API.info("Now with IV");
+
+var iv = mod.randomForIV();
+Ti.API.info('Random IV = ' + iv);
+
+var encyptedText2 = crypto.encrypt(password,plainText,iv);
+Ti.API.info('encyptedText = ' + encyptedText2);
+
+var decryptedText2 = crypto.decrypt(password, encyptedText2,iv);
+Ti.API.info('decryptedText2 = ' + decryptedText2);
+Ti.API.info('Do they match? = ' + ((decryptedText2 == plainText) ? 'Yes' : 'No'));
+
+if(Ti.Platform.Android){
+	Ti.API.info('Output Cross Platform? = ' + ((crypto.decrypt(password, iOSOutput)==decryptedText2) ? 'Yes' : 'No'));
 }
