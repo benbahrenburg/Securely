0954a02b284fdc647603048c37f4d003ecef14de
Jeff Bonnes <jeffb@gameshape.com>
2015-01-06 14:40:59 +1100

    Showing how callbacks / events work on remoteCertificate

--- Samples/Network/app.js
+++ Samples/Network/app.js
@@ -1,23 +1,42 @@
 var win = Ti.UI.createWindow({
-	backgroundColor:'white', layout:'vertical'
+	backgroundColor : 'white',
+	layout : 'vertical'
 });
 
 var securely = require('bencoding.securely');
 Ti.API.info("module is => " + securely);
 
 var cert = securely.createRemoteCertificate({
-    debug:true
+	debug : true
 });
 
 var btnTest = Ti.UI.createButton({
-	title:'Get Remote Thunbprint', top:25, height:45, left:5, right:5	
+	title : 'Get Remote Thumbprint',
+	top : 25,
+	height : 45,
+	left : 5,
+	right : 5
 });
 win.add(btnTest);
 
-btnTest.addEventListener('click',function(x){
-    cert.getThumbprint({
-       url:'https://github.com' 
-    }); 	
+var label = Ti.UI.createLabel({
+	top : 25,
+	left : 5,
+	right : 5,
+	height : 70
+});
+win.add(label);
+
+cert.addEventListener('completed', function(e) {
+	Ti.API.info('thumbprint completed');
+	Ti.API.info('response=' + JSON.stringify(e, null, "\t"));
+	label.text = "SSL Thumbprint=" + e.thumbprint;
+});
+
+btnTest.addEventListener('click', function(x) {
+	cert.getThumbprint({
+		url : 'https://github.com'
+	});
 });
 
 win.open();
